58db4d3e9da1dc1a78ad8a9f1294fa2558d5fcd4
sakkiii <saketmittal.09@gmail.com>
2021-05-13 19:36:49 +0530

    build-check

--- cmd/generic-handlers.go
+++ cmd/generic-handlers.go
@@ -564,8 +564,8 @@ func addCustomHeaders(h http.Handler) http.Handler {
 	})
 }
 
-// addSecurityHeaders adds various HTTP(S) response headers that 
-// enable various security protections behaviors in the client's browser.
+// addSecurityHeaders adds various HTTP(S) response headers.
+// Security Headers enable various security protections behaviors in the client's browser.
 func addSecurityHeaders(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		header := w.Header()
